6badbd45087251d525e147a582b2991875836ede
matuneville <nevillematias@gmail.com>
2025-06-13 15:08:34 -0300

    taller 7 ejercicios 1 y 2

---
+++ talleres/07-verificacion/JML/src/main/java/inge2/dataflow/Absoluto.java
@@ -0,0 +1,17 @@
+package inge2.dataflow;
+
+public class Absoluto {
+
+    // Calcula el valor absoluto de un número entero.
+    // Si el número es negativo, devuelve el opuesto.
+    //
+    //@ requires n != Integer.MIN_VALUE;
+    //@ ensures \result >= 0;
+    public static int valorAbsoluto(int n) {
+        if (n < 0) {
+            return -n;
+        } else {
+            return n;
+        }
+    }
+}
---
+++ talleres/07-verificacion/JML/src/main/java/inge2/dataflow/BusquedaLineal.java
@@ -0,0 +1,23 @@
+package inge2.dataflow;
+
+public class BusquedaLineal {
+
+    // Busca un elemento en un arreglo de enteros.
+    //
+    //@ requires arr != null;
+    //@ ensures (\result <==> (\exists int i; 0 <= i && i < arr.length; arr[i] == elem));
+    public static boolean busquedaLineal(int elem, int[] arr) {
+        boolean result = false;
+
+        //@ loop_invariant 0 <= i && i <= arr.length;
+        //@ loop_invariant result <==> (\exists int j; 0 <= j && j < i; arr[j] == elem);
+        //@ decreasing arr.length - i
+        for (int i = 0; i < arr.length; i++) {
+            if (elem == arr[i]) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+}
